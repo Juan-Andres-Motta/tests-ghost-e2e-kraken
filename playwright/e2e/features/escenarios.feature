c9f4b3e129ab214170a20b1355a7981e20f9093c
Andres Duque <andduque.ing@gmail.com>
2024-05-05 22:57:45 -0500

    feat: Add scenarios for creating, updating, and deleting tags

--- playwright/e2e/features/escenarios.feature
+++ playwright/e2e/features/escenarios.feature
@@ -21,17 +21,37 @@ Feature: Tests playwright
 
 
 
-    Scenario Outline: Create Tag 
+    Scenario Outline: Create Tag
+        When Login
+        Then I click on Tags button 4
+        Then I click on new Tag button 4
+        Then I enter a tag name input 4
+        Then I enter a tag color input 4
+        Then I enter a slug input 4
+        Then I enter a description input 4
+        Then I click on Save button 4
+        Then I click on back to tags 4
+        Then I find the tag created 4
 
-
-
-    Scenario Outline: Update Tag 
-
-
+    Scenario Outline: Update Tag
+        When Login
+        Then I click on tags button 5
+        Then I click on a random tag 5
+        Then I clear and enter a tag name input 5
+        Then I clear and enter a tag color input 5
+        Then I clear and enter a slug input 5
+        Then I clear and enter a description input 5
+        Then I click on Save button 5
+        Then I click on back to tags 5
+        Then I find the tag created 5
 
     Scenario Outline: Delete Tag
-
-
+        When Login
+        Then I click on tags button 6
+        Then I click on a random tag 6
+        Then I click on Delete button 6
+        Then I click the confirm delete button tag 6
+        Then I find the tag deleted 6
 
     Scenario Outline: Create Page
 
@@ -46,5 +66,8 @@ Feature: Tests playwright
 
 
     Scenario Outline: Change Base Color
-
-    
+        When Login
+        Then I click on Dashboard button 10
+        Then I click on customize your design button 10
+        Then I clear and enter an accent color input 10
+        Then I click on Save button 10
--- playwright/e2e/stepDefinitions/escenarios.step.ts
+++ playwright/e2e/stepDefinitions/escenarios.step.ts
@@ -5,6 +5,9 @@ const { faker } = require("@faker-js/faker");
 
 let TITLE = faker.word.words(3);
 let BODY = faker.lorem.paragraph(2);
+let NAME4 = faker.lorem.word();
+let NAME5 = faker.lorem.word();
+let NAME6 = "";
 
 // Scenario: Create a new post
 
@@ -79,48 +82,251 @@ Then("I click on back to posts 1", async function (this: IPlaywrightWorld) {
   await this.page.waitForTimeout(2000);
 });
 
-Then('Find post 1', async function (this: IPlaywrightWorld) {
-    let h3Found = false;
-    let publishButton = await this.page.$$('h3')
-    for (let element of publishButton) {
-        if (await element.innerText() == TITLE) {
-            h3Found = true;
-            break;
-        }
+Then("Find post 1", async function (this: IPlaywrightWorld) {
+  let h3Found = false;
+  let publishButton = await this.page.$$("h3");
+  for (let element of publishButton) {
+    if ((await element.innerText()) == TITLE) {
+      h3Found = true;
+      break;
     }
-    expect(h3Found).toBeTruthy();   
+  }
+  expect(h3Found).toBeTruthy();
 });
 
 // Scenario: Create a new post
 
+// Scenario: Create a new post
 
+// Scenario: Create a new tag
+
+Then("I click on Tags button 4", async function (this: IPlaywrightWorld) {
+  let tagsButton = await this.page.$('a[data-test-nav="tags"]');
+  if (tagsButton) {
+    await tagsButton.click();
+  }
+  await this.page.waitForTimeout(2000);
+});
+
+Then("I click on new Tag button 4", async function (this: IPlaywrightWorld) {
+  let newTagButton = await this.page.$('a[href="#/tags/new/"]');
+  if (newTagButton) {
+    await newTagButton.click();
+  }
+  await this.page.waitForTimeout(2000);
+});
+
+Then("I enter a tag name input 4", async function (this: IPlaywrightWorld) {
+  let tagNameInput = await this.page.$('input[data-test-input="tag-name"]');
+  if (tagNameInput) {
+    await tagNameInput.fill(NAME4);
+  }
+  await this.page.waitForTimeout(2000);
+});
+
+Then("I enter a tag color input 4", async function (this: IPlaywrightWorld) {
+  let tagColorInput = await this.page.$('input[data-test-input="accentColor"]');
+  if (tagColorInput) {
+    await tagColorInput.fill(faker.internet.color().slice(1, 7));
+  }
+  await this.page.waitForTimeout(2000);
+});
+
+Then("I enter a slug input 4", async function (this: IPlaywrightWorld) {
+  let slugInput = await this.page.$('input[data-test-input="tag-slug"]');
+  if (slugInput) {
+    await slugInput.fill(faker.lorem.word());
+  }
+  await this.page.waitForTimeout(2000);
+});
+
+Then("I enter a description input 4", async function (this: IPlaywrightWorld) {
+  let descriptionInput = await this.page.$(
+    'textarea[data-test-input="tag-description"]'
+  );
+  if (descriptionInput) {
+    await descriptionInput.fill(faker.lorem.sentence());
+  }
+  await this.page.waitForTimeout(2000);
+});
+
+Then("I click on Save button 4", async function (this: IPlaywrightWorld) {
+  let saveButton = await this.page.$('button[data-test-button="save"]');
+  if (saveButton) {
+    await saveButton.click();
+  }
+  await this.page.waitForTimeout(2000);
+});
+
+Then("I click on back to tags 4", async function (this: IPlaywrightWorld) {
+  let backToTagsButton = await this.page.$('a[href="#/tags/"]');
+  if (backToTagsButton) {
+    await backToTagsButton.click();
+  }
+  await this.page.waitForTimeout(2000);
+});
+
+Then("I find the tag created 4", async function (this: IPlaywrightWorld) {
+  let h3Found = false;
+  let tagCreated = await this.page.$$("h3");
+  for (let element of tagCreated) {
+    if ((await element.innerText()) == NAME4) {
+      h3Found = true;
+      break;
+    }
+  }
+  expect(h3Found).toBeTruthy();
+  await this.page.waitForTimeout(2000);
+});
+
+// Scenario: Update a tag
+
+Then("I click on tags button 5", async function (this: IPlaywrightWorld) {
+  await this.page.click('a[href="#/tags/"]');
+  await this.page.waitForTimeout(1000);
+});
+
+Then("I click on a random tag 5", async function (this: IPlaywrightWorld) {
+  let tags = await this.page.$$("h3");
+  let randomTag = tags[Math.floor(Math.random() * tags.length)];
+  await randomTag.click();
+  await this.page.waitForTimeout(1000);
+});
+
+Then(
+  "I clear and enter a tag name input 5",
+  async function (this: IPlaywrightWorld) {
+    let tagNameInput = await this.page.$('input[data-test-input="tag-name"]');
+    await tagNameInput!.fill(NAME5);
+    await this.page.waitForTimeout(1000);
+  }
+);
+
+Then(
+  "I clear and enter a tag color input 5",
+  async function (this: IPlaywrightWorld) {
+    let tagColorInput = await this.page.$(
+      'input[data-test-input="accentColor"]'
+    );
+    await tagColorInput!.fill(faker.internet.color().slice(1, 7));
+    await this.page.waitForTimeout(1000);
+  }
+);
+
+Then(
+  "I clear and enter a slug input 5",
+  async function (this: IPlaywrightWorld) {
+    let slugInput = await this.page.$('input[data-test-input="tag-slug"]');
+    await slugInput!.fill(faker.lorem.word());
+    await this.page.waitForTimeout(1000);
+  }
+);
+
+Then(
+  "I clear and enter a description input 5",
+  async function (this: IPlaywrightWorld) {
+    let descriptionInput = await this.page.$(
+      'textarea[data-test-input="tag-description"]'
+    );
+    await descriptionInput!.fill(faker.lorem.sentence());
+    await this.page.waitForTimeout(1000);
+  }
+);
+
+Then("I click on Save button 5", async function (this: IPlaywrightWorld) {
+  let saveButton = await this.page.$('button[data-test-button="save"]');
+  await saveButton!.click();
+  await this.page.waitForTimeout(1000);
+});
+
+Then("I click on back to tags 5", async function (this: IPlaywrightWorld) {
+  let backToTagsButton = await this.page.$('a[href="#/tags/"]');
+  await backToTagsButton!.click();
+  await this.page.waitForTimeout(1000);
+});
+
+Then("I find the tag created 5", async function (this: IPlaywrightWorld) {
+  let h3Found = false;
+  let tagCreated = await this.page.$$("h3");
+  for (let element of tagCreated) {
+    if ((await element.innerText()) == NAME5) {
+      h3Found = true;
+      break;
+    }
+  }
+  expect(h3Found).toBeTruthy();
+  await this.page.waitForTimeout(1000);
+});
+
+// Scenario: Delete a tag
+
+Then("I click on tags button 6", async function (this: IPlaywrightWorld) {
+  await this.page.click('a[href="#/tags/"]');
+  await this.page.waitForTimeout(1000);
+});
+
+Then("I click on a random tag 6", async function (this: IPlaywrightWorld) {
+  let tags = await this.page.$$("h3");
+  let randomTag = tags[Math.floor(Math.random() * tags.length)];
+  NAME6 = await randomTag.innerText();
+  await randomTag.click();
+  await this.page.waitForTimeout(1000);
+});
+
+Then("I click on Delete button 6", async function (this: IPlaywrightWorld) {
+  let deleteButton = await this.page.$('button[data-test-button="delete-tag"]');
+  await deleteButton!.click();
+  await this.page.waitForTimeout(1000);
+});
+
+Then(
+  "I click the confirm delete button tag 6",
+  async function (this: IPlaywrightWorld) {
+    let confirmButton = await this.page.$('button[data-test-button="confirm"]');
+    await confirmButton!.click();
+    await this.page.waitForTimeout(1000);
+  }
+);
+
+Then("I find the tag deleted 6", async function (this: IPlaywrightWorld) {
+  let h3Found = false;
+  let tagCreated = await this.page.$$("h3");
+  for (let element of tagCreated) {
+    if ((await element.innerText()) == NAME6) {
+      h3Found = true;
+      break;
+    }
+  }
+  expect(h3Found).toBeFalsy();
+  await this.page.waitForTimeout(1000);
+});
 
 // Scenario: Create a new post
 
-
+// Scenario: Create a new post
 
 // Scenario: Create a new post
 
+Then("I click on Dashboard button 10", async function (this: IPlaywrightWorld) {
+  await this.page.click('a[data-test-nav="dashboard"]');
+  await this.page.waitForTimeout(1000);
+});
 
+Then("I click on customize your design button 10", async function (this: IPlaywrightWorld) {
+  await this.page.click('a[href="#/settings/design/edit/?ref=setup"]');
+  await this.page.waitForTimeout(1000);
+});
 
-// Scenario: Create a new post
+Then("I clear and enter an accent color input 10", async function (this: IPlaywrightWorld) {
+  let tagColorInput = await this.page.$('input[aria-label="Color value"]');
+  await tagColorInput!.fill(faker.internet.color().slice(1, 7));
+  await this.page.waitForTimeout(1000);
+});
 
-
-
-// Scenario: Create a new post
-
-
-
-// Scenario: Create a new post
-
-
-
-// Scenario: Create a new post
-
-
-
-// Scenario: Create a new post
-
-
-
-// Scenario: Create a new post
+Then("I click on Save button 10", async function (this: IPlaywrightWorld) {
+  let saveButton = await this.page.$(
+    'button[class="cursor-pointer bg-black text-white dark:bg-white dark:text-black hover:bg-grey-900 inline-flex items-center justify-center whitespace-nowrap rounded text-sm transition font-bold h-[34px] px-4"]'
+  );
+  await saveButton!.click();
+  await this.page.waitForTimeout(1000);
+});
